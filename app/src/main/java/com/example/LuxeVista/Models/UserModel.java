259485aab9d62c193283f0f5d9d4b5d23d56e07f
Rehan <dhanujarehan@gmail.com>
2025-07-26 23:00:36 +0530

    UserModel.java updated

--- app/src/main/java/com/example/LuxeVista/Models/UserModel.java
+++ app/src/main/java/com/example/LuxeVista/Models/UserModel.java
@@ -2,15 +2,16 @@ package com.example.LuxeVista.Models;
 
 public class UserModel {
     private int id;
-    private String firstName, lastName, email, phone, nic;
+    private String firstName, lastName, email, phone, nic, password;
 
-    public UserModel(int id, String firstName, String lastName, String email, String phone, String nic) {
+    public UserModel(int id, String firstName, String lastName, String email, String phone, String nic, String password) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phone = phone;
         this.nic = nic;
+        this.password = password;
     }
 
     public int getId() { return id; }
@@ -19,5 +20,5 @@ public class UserModel {
     public String getEmail() { return email; }
     public String getPhone() { return phone; }
     public String getNic() { return nic; }
+    public String getPassword() { return password; }
 }
-
